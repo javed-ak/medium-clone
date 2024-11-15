25366f60c23ff11e2cf7b3ab1ad68ef98c44af4a
Javed Akhtar <conn.javed@gmail.com>
2024-11-15 17:51:36 +0530

    fe

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
 import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 export interface BlogProps {
     id: string;
@@ -14,44 +15,54 @@ export interface BlogProps {
 export const useBlog = ({ id } : { id:string }) => {
     const [loading, setLoading] = useState(true);
     const [blog, setBlog] = useState<BlogProps>();
+    const navigate = useNavigate()
 
-    useEffect(() => {
-        // getBlog();
-        axios.get(`${BACKEND_URL}/api/v1/blog/${id}`, {
-            headers: {
-                Authorization: localStorage.getItem('token')
-            }
-        })
-        .then(reponse => {
-            setBlog(reponse.data.blog)
-            setLoading(false)
-        })
-    }, [id])
-
-    return {
-        loading,
-        blog
+    try{
+        useEffect(() => {
+            // getBlog();
+            axios.get(`${BACKEND_URL}/api/v1/blog/${id}`, {
+                headers: {
+                    Authorization: localStorage.getItem('token')
+                }
+            })
+            .then(reponse => {
+                setBlog(reponse.data.blog)
+                setLoading(false)
+            })
+        }, [id])
+    
+        return {
+            loading,
+            blog
+        }
+    } catch (e) {
+        navigate('/signin')
     }
 }
 
 export const useBlogs = () => {
     const [loading, setLoading] = useState(true);
     const [blogs, setBlogs] = useState<BlogProps[]>([]);
+    const navigate = useNavigate()
 
-    useEffect(() => {
-        axios.get(`${BACKEND_URL}/api/v1/blog/bulk`,{
-            headers: {
-                Authorization: localStorage.getItem('token')
-            }
-        })
-        .then(response => {
-            setBlogs(response.data.blogs);
-            setLoading(false)
-        })
-    }, [])
-
-    return {
-        loading,
-        blogs
+    try{
+        useEffect(() => {
+            axios.get(`${BACKEND_URL}/api/v1/blog/bulk`,{
+                headers: {
+                    Authorization: localStorage.getItem('token')
+                }
+            })
+            .then(response => {
+                setBlogs(response.data.blogs);
+                setLoading(false)
+            })
+        }, [])
+    
+        return {
+            loading,
+            blogs
+        }
+    } catch (e) {
+        navigate('/signin')
     }
 }
