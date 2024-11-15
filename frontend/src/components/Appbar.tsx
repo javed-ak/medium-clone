509e98ba3d5f7fb14c3f40f52b4338bc6692b65e
Javed Akhtar <conn.javed@gmail.com>
2024-11-15 18:04:15 +0530

    fe

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,8 +1,14 @@
 import { Avatar } from "./BlogCard"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { motion } from 'motion/react'
 
 export const Appbar = () => {
+    const navigate = useNavigate()
+    const handleLogout = () => {
+        localStorage.clear();
+        navigate("/signup");
+    };
+    
     return (
         <div className="flex justify-between items-center bg-zinc-50 px-10 py-4 border-b sticky top-0 z-20">
             <Link to={'/blogs'}>
@@ -14,9 +20,7 @@ export const Appbar = () => {
                 <Link to={'/publish'}>
                     <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="ring ring-green-500 text-slate-50 bg-green-600 rounded-full font-bold px-5 py-1">New</motion.button>
                 </Link>
-                <Link to={'/signup'}>
-                    <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} className="ring ring-green-500 text-slate-50 bg-green-600 rounded-full font-bold px-5 py-1">Logout</motion.button>
-                </Link>
+                <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }} onClick={handleLogout} className="ring ring-green-500 text-slate-50 bg-green-600 rounded-full font-bold px-5 py-1">Logout</motion.button>
                 <Avatar name="Javed" size='big'/>
             </div>
         </div>
